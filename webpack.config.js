c0489a8fed8b615d42694855bf137757e765fae5
Vedanth Bora <vedanthbora@gmail.com>
2022-04-27 20:26:03 +0530

    [FEAT]: cra with webpack, prettier, eslint , babel

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-undef */
+
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "main.js",
+  },
+
+  target: "web",
+  devServer: {
+    port: "6060",
+    static: ["./public"],
+    open: true,
+    hot: true,
+    liveReload: true,
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json", ".ts"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
+};
